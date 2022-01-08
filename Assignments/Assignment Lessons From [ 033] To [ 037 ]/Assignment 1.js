9d214c667fbc54158187ec70c0f52cbea53f86d5
AhmmedAde <ahmedadelelzero@gmail.com>
2022-01-08 16:00:57 +0200

    Upload Assignment

---
+++ Assignments/Assignment Lessons From [ 033] To [ 037 ]/Assignment 1.js
@@ -0,0 +1,20 @@
+// Test Case 1
+let numOne = 9; // "009"
+
+if (numOne < 10) {
+  console.log(`00${numOne}`);
+}
+
+// Test Case 2
+let numTwo = 20; // "020"
+
+if (numTwo > 10 && numTwo < 100) {
+  console.log(`0${numTwo}`);
+}
+
+// Test Case 3
+let num3 = 110; // "110"
+
+if (num3 >= 100) {
+  console.log(num3);
+}
---
+++ Assignments/Assignment Lessons From [ 033] To [ 037 ]/Assignment 4.js
@@ -0,0 +1,53 @@
+let num1 = 15;
+let num2 = 1;
+let num3 = +"15";
+let num4 = 50;
+
+// Condition 1
+if (num1 > num2) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// Condition 2
+if (num1 > num2 && num1 < num4) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// Condition 3
+if (num1 + num2 && num1 === num3) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// Condition 4
+if (num1 + num3 < num4) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// Condition 5
+if (num1 + num3 < num4) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// Condition 6
+if (num1 + num2 + num3 < num4) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// Condition 7
+if (num4 - (num1 + num3) + num2 === 21) {
+  console.log(true);
+} else {
+  console.log(false);
+}
