7410297f51442af712d4490e8c6152d149dcb2bb
AhmmedAde <ahmedadelelzero@gmail.com>
2022-02-07 20:21:12 +0200

    Upload Assignment

---
+++ Assignments/Assignment Lessons From [ 054 ] To [ 056 ]/Assignment 1.js
@@ -0,0 +1,15 @@
+let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
+let index = 0;
+let counter = 0;
+
+while (index < friends.length) {
+  if (typeof friends[index] !== "number" && !friends[index].startsWith("A")) {
+    console.log(`${counter + true} => ${friends[index]}`);
+    counter++;
+  }
+  index++;
+}
+
+// Output
+("1 => Sayed");
+("1 => Mahmoud");
