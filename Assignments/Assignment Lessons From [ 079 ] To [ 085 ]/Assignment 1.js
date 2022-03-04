b660fd2f7e36584611f15b2ab9301be49e8f8900
AhmmedAde <ahmedadelelzero@gmail.com>
2022-03-04 13:57:06 +0200

    Upload Assignment

--- Assignments/Assignment Lessons From [ 079 ] To [ 085 ]/Assignment 1.js
+++ Assignments/Assignment Lessons From [ 079 ] To [ 085 ]/Assignment 1.js
@@ -1,10 +1,11 @@
 let member = {
-  name: "Elzero",
-  age: 38,
-  country: "Egypt",
-  fullDetails: function () {
-    return `My Name Is ${member.name}, My Age Is ${member.age}, I Live In ${member.country}`;
-  },
-};
-
-console.log(member.fullDetails()); // My Name Is Elzero, My Age Is 38, I Live In Egypt
+    name: "Elzero",
+    age: 38,
+    country: "Egypt",
+    fullDetails: function () {
+      return `My Name Is ${member.name}, My Age Is ${member.age}, I Live In ${member.country}`;
+    },
+  };
+  
+  console.log(member.fullDetails()); // My Name Is Elzero, My Age Is 38, I Live In Egypt
+  
